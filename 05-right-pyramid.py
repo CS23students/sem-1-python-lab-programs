0f43af942097fbc2ffdbf19ea06044da58b5734f
CS23students <153345000+CS23students@users.noreply.github.com>
2024-02-22 20:59:44 +0530

    Update 05-right-pyramid.py

--- 05-right-pyramid.py
+++ 05-right-pyramid.py
@@ -6,8 +6,7 @@ for i in range(0, n):
     for j in range(0, i+1):
         print("*", end=" ")
     print()
-
-for i in range(n-1, 0, -1):
+for i in range(n, 0, -1):
     for j in range(0, i):
         print("*", end=" ")
     print()
